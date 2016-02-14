e5bd810b4a7afeee18c700f6a83d1b253e198dfc
yuhua he <yuhuahe@buffalo.edu>
2016-02-14 15:48:25 -0800

    first commit

---
+++ components/AppController.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class AppController extends React.Component {
+  render() {
+    return <div>Start working222</div>
+  }
+}
+
+export default AppController;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
