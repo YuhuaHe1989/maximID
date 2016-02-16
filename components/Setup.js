b7c4848ebdcf319ee75b59cb206e97a9099a5772
yuhua he <yuhuahe@buffalo.edu>
2016-02-15 22:36:53 -0800

    setup style bug fixed

--- components/Setup.js
+++ components/Setup.js
@@ -3,27 +3,28 @@ import React from 'react'
 export default React.createClass({
   render() {
     return (
-      <div id="page-wrapper" className="gray-bg">
+      <div id="wrapper">
+    <div id="page-wrapper" className="gray-bg">
         <div className="row border-bottom">
-                    <nav className="navbar navbar-static-top white-bg" role="navigation" >
-                        <div className="navbar-header">
-                            <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i className="fa fa-bars"></i> </a>
-                            <form role="search" className="navbar-form-custom" method="post" action="search_results.html">
-                                <div className="form-group">
-                                    <input type="text" placeholder="Setup" className="form-control" name="top-search" id="top-search"/>
-                                </div>
-                            </form>
+            <nav className="navbar navbar-static-top white-bg" role="navigation" style={{marginBottom: "0"}}>
+                <div className="navbar-header">
+                    <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i className="fa fa-bars"></i> </a>
+                    <form role="search" className="navbar-form-custom" method="post" action="search_results.html">
+                        <div className="form-group">
+                            <input type="text" placeholder="Setup" className="form-control" name="top-search" id="top-search"/>
                         </div>
-                        <ul className="nav navbar-top-links navbar-right">
-                            <li>
-                                <a href="login.html">
-                                    <i className="fa fa-sign-out"></i> Log out
-                                </a>
-                            </li>
-                        </ul>
-
-                    </nav>
+                    </form>
                 </div>
+                <ul className="nav navbar-top-links navbar-right">
+                    <li>
+                        <a href="login.html">
+                            <i className="fa fa-sign-out"></i> Log out
+                        </a>
+                    </li>
+                </ul>
+
+            </nav>
+        </div>
         <div className="wrapper wrapper-content animated fadeInRight">
 
             <div className="row">
@@ -53,29 +54,29 @@ export default React.createClass({
                             <form method="get" className="form-horizontal">
                                 <div className="form-group"><label className="col-sm-2 control-label">Server URL</label>
 
-                                    <div className="col-sm-10"><input type="text" className="form-control" value="https://wf.maximID.com/fido" /></div>
+                                    <div className="col-sm-10"><input type="text" className="form-control" value="https://wf.maximID.com/fido"/></div>
                                 </div>
                                 <div className="hr-line-dashed"></div>
                                 <div className="form-group"><label className="col-sm-2 control-label">Console Admin User Repository</label>
-                                    <div className="col-sm-10"><input type="text" className="form-control" value="CorpLDAP" /> <span className="help-block m-b-none">Specify where to authenticate top-level admins from.</span>
+                                    <div className="col-sm-10"><input type="text" className="form-control" value="CorpLDAP"/> <span className="help-block m-b-none">Specify where to authenticate top-level admins from.</span>
                                     </div>
                                 </div>
                                 <div className="hr-line-dashed"></div>
                                 <div className="form-group"><label className="col-sm-2 control-label">Default Alert Emailid/Alias</label>
 
-                                    <div className="col-sm-10"><input type="text" placeholder="email@example.com" className="form-control" /></div>
+                                    <div className="col-sm-10"><input type="text" placeholder="email@example.com" className="form-control"/></div>
                                 </div>
                                 <div className="hr-line-dashed"></div>
                                 <div className="form-group"><label className="col-sm-2 control-label">Maximum users per Customer</label>
 
-                                    <div className="col-sm-10"><input type="text" placeholder="100000" className="form-control" /></div>
+                                    <div className="col-sm-10"><input type="text" placeholder="100000" className="form-control"/></div>
                                 </div>
                                 <div className="hr-line-dashed"></div>
                                 <div className="form-group"><label className="col-sm-2 control-label">Enable Context data <br/>
                                     </label>
 
                                     <div className="col-sm-10">
-                                        <div className="checkbox"><label> <input type="checkbox" value="" /></label></div>
+                                        <div className="checkbox"><label> <input type="checkbox" value=""/></label></div>
                                     </div>
                                 </div>
                                 <div className="hr-line-dashed"></div>
@@ -83,7 +84,7 @@ export default React.createClass({
                                     </label>
 
                                     <div className="col-sm-10">
-                                        <div className="checkbox"><label> <input type="checkbox" value="" /></label></div>
+                                        <div className="checkbox"><label> <input type="checkbox" value=""/></label></div>
                                     </div>
                                 </div>
                                 <div className="hr-line-dashed"></div>
@@ -91,7 +92,7 @@ export default React.createClass({
                                     </label>
 
                                     <div className="col-sm-10">
-                                        <div className="checkbox"><label> <input type="checkbox" value="" /></label></div>
+                                        <div className="checkbox"><label> <input type="checkbox" value=""/></label></div>
                                     </div>
                                 </div>
                                 <div className="hr-line-dashed"></div>
@@ -118,7 +119,10 @@ export default React.createClass({
                 <strong>Copyright</strong> maximID Inc &copy; 2014-2015
             </div>
         </div>
-      </div>
+
+    </div>
+</div>
+
       )
   }
 })
